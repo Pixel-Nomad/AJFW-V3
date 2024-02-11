d5353a8271166640f912e6bd358e7de71891c4d3
9dgamer <mr.tgamer247797703@gmail.com>
2024-02-11 14:48:13 +0500

    Game/Chairs : Added

---
+++ server/resources/[aj3.0]/[jim]/aj-chairs/fxmanifest.lua
@@ -0,0 +1,11 @@
+name "aj-Chairs"
+author ""
+version "1.5"
+description "aj-Chairs By "
+fx_version "cerulean"
+game "gta5"
+lua54 'yes'
+
+shared_scripts { 'config.lua', 'shared/*.lua' }
+client_scripts { 'client/*.lua' }
+server_scripts { 'server/*.lua' }
---
+++ server/resources/[aj3.0]/[jim]/aj-chairs/server/server.lua
@@ -0,0 +1,20 @@
+local AJFW = exports[Config.Core]:GetCoreObject()
+RegisterNetEvent('AJFW:Server:UpdateObject', function() if source ~= '' then return false end AJFW = exports[Config.Core]:GetCoreObject() end)
+
+CreateThread(function()
+	if Config.Inv == "ox" then for k, v in pairs(Config.Stores) do exports.ox_inventory:RegisterShop("ChairStore"..k, { name = v.label, inventory = v.items}) end end
+	for i=1, 110, 1 do
+		if AJFW.Shared.Items["chair"..i] then	AJFW.Functions.CreateUseableItem("chair"..i, function(source, item) TriggerClientEvent('aj-chairs:Use', source, i) end) end
+	end
+end)
+local function CheckVersion()
+	PerformHttpRequest('https://raw.githubusercontent.com/jimathy/aj-chairs/master/version.txt', function(err, newestVersion, headers)
+		local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+		if not newestVersion then print("Currently unable to run a version check.") return end
+		local advice = "^1You are currently running an outdated version^7, ^1please update"
+		if newestVersion:gsub("%s+", "") == currentVersion:gsub("%s+", "") then advice = '^6You are running the latest version.'
+		else print("^3Version Check^7: ^2Current^7: "..currentVersion.." ^2Latest^7: "..newestVersion) end
+		print(advice)
+	end)
+end
+CheckVersion()
