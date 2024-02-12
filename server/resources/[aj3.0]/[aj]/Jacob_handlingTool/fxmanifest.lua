897ed1e5e40a80012819cf4a1c55b3ec97113bf0
9dgamer <mr.tgamer247797703@gmail.com>
2024-02-12 19:45:12 +0500

    Vehicle/HandlingTool : Added

---
+++ server/resources/[aj3.0]/[aj]/Jacob_handlingTool/fxmanifest.lua
@@ -0,0 +1,31 @@
+author '9dgamer'
+
+description 'Utility to edit the handling of vehicles and record speeds'
+
+version '1.0.0'
+
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.css',
+	'html/index.html',
+	'html/index.js',
+}
+
+client_scripts {
+	'cl_config.lua',
+	'cl_debugger.lua',
+}
+
+escrow_ignore{
+	'html/index.css',
+	'html/index.html',
+	'html/index.js',
+}
+
+server_script 'sv_debugger.lua'
