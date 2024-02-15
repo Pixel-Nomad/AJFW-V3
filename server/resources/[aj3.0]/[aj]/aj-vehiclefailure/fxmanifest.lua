6f37c4d9c167ebf2c4ed0ea163d8e1439538aa21
9dgamer <mr.tgamer247797703@gmail.com>
2024-02-15 09:39:47 +0500

    vehicle/failure : Oops

--- server/resources/[aj3.0]/[aj]/aj-vehiclefailure/fxmanifest.lua
+++ server/resources/[aj3.0]/[aj]/aj-vehiclefailure/fxmanifest.lua
@@ -5,7 +5,7 @@ description 'QB-VehicleFailure'
 version '1.2.2'
 
 shared_scripts {
-    '@qb-core/shared/locale.lua',
+    '@aj-base/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
     'config.lua'
