1b320cd8a15f1be175e44d6786d3592f129c2d67
9dgamer <mr.tgamer247797703@gmail.com>
2024-02-15 17:17:26 +0500

    game/elevators : Added

---
+++ server/resources/[aj3.0]/[wasabi]/aj-elevators/fxmanifest.lua
@@ -0,0 +1,25 @@
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+
+
+version '1.0.2'
+
+client_scripts {
+	'client/*.lua',
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'config.lua'
+}
+
+dependencies {
+	'ox_lib'
+}
