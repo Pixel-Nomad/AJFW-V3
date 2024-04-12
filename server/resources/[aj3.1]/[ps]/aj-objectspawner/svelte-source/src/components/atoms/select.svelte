692ae598807d26895a91509f3b43d837612277f9
Pixel Nomad <56701817+Pixel-Nomad@users.noreply.github.com>
2024-04-12 16:40:49 +0500

    Framework/ObjectSpawner: Added

---
+++ server/resources/[aj3.1]/[ps]/aj-objectspawner/svelte-source/src/components/atoms/select.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import Select from "svelte-select";
+
+  export let valuesArray: Array<any> = [""]
+  export let handleSelectFunction: (val) => void = (val) => null;
+  export let value: any = null;
+  export let placeHolder: string = "Search...";
+  export let virtualList: boolean = false;
+
+  function handleSelect(event) {
+    handleSelectFunction(event.detail.value);
+  }
+
+</script>
+
+<div class="themed text-white text-base">
+  <Select items={valuesArray} labelIdentifier={"value"} {value} on:select={handleSelect} placeholder={placeHolder} isVirtualList={virtualList}/>
+</div>
+
+<style>
+  .themed {
+    --background: #292929;
+    --listBackground: #292929;
+    --itemIsActiveBG: var(--aj-primary);
+    --itemIsActiveColor: black;
+    --itemHoverBG: var(--aj-primary-hover);
+    --border: #171717;
+    --placeholderColor: #ffffff;
+    --inputColor: #ffffff;
+    --inputFontSize: 1rem;
+    --clearSelectFocusColor: var(--aj-primary);
+    --clearSelectColor: var(--aj-primary);
+    --clearSelectHoverColor: var(--aj-primary);
+    --listEmptyColor: #ffffff;
+  }
+  :global(.selectContainer > *) {
+    cursor: pointer !important;
+  }
+  :global(.listItem > *) {
+    cursor: pointer !important;
+  }
+  :global(.listContainer) {
+    z-index: 3 !important;
+  }
+</style>
