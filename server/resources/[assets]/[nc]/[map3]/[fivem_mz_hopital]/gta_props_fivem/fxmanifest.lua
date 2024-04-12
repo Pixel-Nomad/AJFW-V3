fa1adba67eb8a4226238c0118717848c9d3b03a8
Pixel Nomad <56701817+Pixel-Nomad@users.noreply.github.com>
2024-04-12 16:42:06 +0500

    Assets/manifest: Update

--- server/resources/[assets]/[nc]/[map3]/[fivem_mz_hopital]/gta_props_fivem/fxmanifest.lua
+++ server/resources/[assets]/[nc]/[map3]/[fivem_mz_hopital]/gta_props_fivem/fxmanifest.lua
@@ -1,6 +1,12 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+ 
+description 'AJFW Example Manifest'
+version '1.0.0'
 
 this_is_a_map 'yes'
+
+
 
 data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_02.ytyp'
 
--- server/resources/[assets]/[nc]/[map3]/[fivem_mz_hopital]/urgence_floor/fxmanifest.lua
+++ server/resources/[assets]/[nc]/[map3]/[fivem_mz_hopital]/urgence_floor/fxmanifest.lua
@@ -1,6 +1,12 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+ 
+description 'AJFW Example Manifest'
+version '1.0.0'
 
 this_is_a_map 'yes'
+
+
 
 file 'gusepe_timecycle_mods_1.xml'
 data_file 'TIMECYCLEMOD_FILE' 'gusepe_timecycle_mods_1.xml'
