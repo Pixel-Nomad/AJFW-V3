d99519d45ab0922ef0c484c315ddc5b6e8c8eb19
Pixel Nomad <mr.tgamer247797703@gmail.com>
2024-06-11 13:34:23 +0500

    *hidden stuff*

---
+++ server/resources/[assets]/[cars]/[data]/a_assets_dlc_bravo/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+name 'Car Pack Handling'
+version '1.0'
+description 'Car Pack Handling'
+ 
+files{
+    'data/**/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
---
+++ server/resources/[assets]/[cars]/[stream]/a_assets_vehicles_pack11/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+name 'Car Pack'
+version '1.0'
+description 'Car Pack'
