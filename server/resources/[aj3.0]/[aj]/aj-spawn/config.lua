3046d3adcd3b2d6139ec909798ac53535d6e7454
Pixel Nomad <mr.tgamer247797703@gmail.com>
2024-06-16 09:46:42 +0500

    Framework/spawns: Added 4 new spawns

--- server/resources/[aj3.0]/[aj]/aj-spawn/config.lua
+++ server/resources/[aj3.0]/[aj]/aj-spawn/config.lua
@@ -6,85 +6,49 @@ AJ.Spawns = {
         coords = vector4(195.17, -933.77, 29.7, 144.5),
         location = "legion",
         label = "Legion Square",
-        pos = {top = 50.9, left = 65.5}
+        pos = {top = 51.3, left = 67.5}
     },
     ["paleto"] = {
         coords = vector4(80.35, 6424.12, 31.67, 45.5),
         location = "paleto",
         label = "Paleto Bay",
-        pos = {top = 53.9, left = 29.0}
+        pos = {top = 56.4, left = 22.7}
     },
     ["trainstation"] = {
         coords = vector4(-207.15, -1016.67, 30.14, 32.7),
         location = "trainstation",
         label = "Train Station",
-        pos = {top = 54.6, left = 66.5}
+        pos = {top = 55.7, left = 68.1}
     },
     ["busstation"] = {
         coords = vector4(435.84, -646.05, 28.74, 97.62),
         location = "busstation",
         label = "Bus Station",
-        pos = {top = 48.9, left = 64.1}
+        pos = {top = 48.1, left = 66.5}
     },
-    ["police"] = {
-        coords = vector4(434.76, -976.89, 30.72, 4.88),
-        location = "police",
-        label = "M.R.P.D",
-        pos = {top = 48.3, left = 66.2}
+    ["pinkcage"] = {
+        coords = vector4(324.88, -229.7, 54.22, 157.76),
+        location = "pinkcage",
+        label = "Pink Cage Motel",
+        pos = {top = 49.7, left = 63.1}
     },
-    ["doctor"] = {
-        coords = vector4(299.02, -573.97, 43.26, 125.61),
-        location = "doctor",
-        label = "Pillbox",
-        pos = {top = 49.9, left = 64.1}
+    ["perrera"] = {
+        coords = vector4(-1483.39, -674.67, 28.94, 35.73),
+        location = "perrera",
+        label = "Perrera beach motel",
+        pos = {top = 69.7, left = 66.0}
     },
-    ["government"] = {
-        coords = vector4(-1296.95, -588.06, 29.3, 138.62),
-        location = "government",
-        label = "City Hall",
-        pos = {top = 64.9, left = 63.5}
+    ["crownjewels"] = {
+        coords = vector4(-1319.8, -907.37, 11.31, 209.47),
+        location = "crownjewels",
+        label = "Crown Jewels motel",
+        pos = {top = 68.3, left = 67.5}
     },
-    ["doj"] = {
-        coords = vector4(-546.95, -210.67, 37.65, 217.98),
-        location = "doj",
-        label = "Court House",
-        pos = {top = 57.2, left = 62.5}
-    },
-    ["edm"] = {
-        coords = vector4(174.01, -1118.47, 29.34, 181.88),
-        location = "edm",
-        label = "EDM",
-        pos = {top = 51.6, left = 66.6}
-    },
-    ["pdm"] = {
-        coords = vector4(-45.83, -1104.9, 27.26, 151.06),
-        location = "pdm",
-        label = "PDM",
-        pos = {top = 53.4, left = 67.4}
-    },
-    ["mechanic"] = {
-        coords = vector4(-357.55, -149.5, 38.25, 32.12),
-        location = "mechanic",
-        label = "Mechanic",
-        pos = {top = 55.6, left = 61.5}
-    },
-    ["catcafe"] = {
-        coords = vector4(-605.19, -1041.07, 21.79, 3.5),
-        location = "catcafe",
-        label = "Cat Cafe",
-        pos = {top = 57.6, left = 66.5}
-    },
-    ["burgershot"] = {
-        coords = vector4(-1183.75, -905.23, 13.41, 219.14),
-        location = "burgershot",
-        label = "Burger Shot",
-        pos = {top = 63.2, left = 65.7}
-    },
-    ["realestate"] = {
-        coords = vector4(-722.79, 273.4, 85.65, 292.39),
-        location = "realestate",
-        label = "Real Estate",
-        pos = {top = 58.6, left = 59.7}
+    ["bilingsgate"] = {
+        coords = vector4(561.83, -1773.09, 29.35, 327.99),
+        location = "bilingsgate",
+        label = "Billings Gate Motel",
+        pos = {top = 46.8, left = 72.5}
     },
 }
 
@@ -93,25 +57,49 @@ AJ.SpawnsPlayers = {
         coords = vector4(195.17, -933.77, 29.7, 144.5),
         location = "legion",
         label = "Legion Square",
-        pos = {top = 50.9, left = 65.5}
+        pos = {top = 51.3, left = 67.5}
     },
     ["paleto"] = {
         coords = vector4(80.35, 6424.12, 31.67, 45.5),
         location = "paleto",
         label = "Paleto Bay",
-        pos = {top = 53.9, left = 29.0}
+        pos = {top = 56.4, left = 22.7}
     },
     ["trainstation"] = {
         coords = vector4(-207.15, -1016.67, 30.14, 32.7),
         location = "trainstation",
         label = "Train Station",
-        pos = {top = 54.6, left = 66.5}
+        pos = {top = 55.7, left = 68.1}
     },
     ["busstation"] = {
         coords = vector4(435.84, -646.05, 28.74, 97.62),
         location = "busstation",
         label = "Bus Station",
-        pos = {top = 48.9, left = 64.1}
+        pos = {top = 48.1, left = 66.5}
+    },
+    ["pinkcage"] = {
+        coords = vector4(324.88, -229.7, 54.22, 157.76),
+        location = "pinkcage",
+        label = "Pink Cage Motel",
+        pos = {top = 49.7, left = 63.1}
+    },
+    ["perrera"] = {
+        coords = vector4(-1483.39, -674.67, 28.94, 35.73),
+        location = "perrera",
+        label = "Perrera beach motel",
+        pos = {top = 69.7, left = 66.0}
+    },
+    ["crownjewels"] = {
+        coords = vector4(-1319.8, -907.37, 11.31, 209.47),
+        location = "crownjewels",
+        label = "Crown Jewels motel",
+        pos = {top = 68.3, left = 67.5}
+    },
+    ["bilingsgate"] = {
+        coords = vector4(561.83, -1773.09, 29.35, 327.99),
+        location = "bilingsgate",
+        label = "Billings Gate Motel",
+        pos = {top = 46.8, left = 72.5}
     },
 }
 
